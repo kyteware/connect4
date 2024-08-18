bd2e7afe2804aee5c028f543b51ad944632aea3b
dworv <dwarvyt@gmail.com>
2024-08-18 05:26:39 -0700

    progress on minmax

--- connect4/src/lib.rs
+++ connect4/src/lib.rs
@@ -1,10 +1,69 @@
+#[derive(Clone, Default)]
 pub struct Position {
-    /// row major (im not about to bike shed bit encoding this shit)
-    pub grid: [[Slot; 6]; 7]
+    /// row major,
+    /// rows top to bottom
+    /// cols left to right 
+    /// 
+    /// (im not about to bike shed bit encoding this shit)
+    pub grid: [[Slot; 7]; 6]
 }
 
+impl Position {
+    pub fn get(&self, row: usize, col: usize) -> Slot {
+        self.grid[row][col]
+    }
+
+    pub fn with_move(&self, color: Color, col: usize) -> Position {
+        for row in (0..6).rev() {
+            if self.get(row, col) == Slot::Empty {
+                let mut new_pos = self.clone();
+                new_pos.grid[row][col] = Slot::Color(color);
+
+                return new_pos;
+            }
+        }
+
+        panic!("row already full") // stfu this is incredibly idiomatic
+    }
+
+    pub fn allowed_moves(&self) -> Vec<usize> {
+        let mut moves = vec![];
+
+        for c in 0..7 {
+            if self.get(0, c) == Slot::Empty {
+                moves.push(c);
+            }
+        }
+
+        moves
+    }
+}
+
+#[derive(Clone, Copy, Debug, Default, PartialEq, Eq)]
 pub enum Slot {
+    #[default]
     Empty,
+    Color(Color)
+}
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum Color {
     Red,
     Yellow
+}
+
+impl Color {
+    pub fn opposite(self) -> Color {
+        match self {
+            Color::Red => Color::Yellow,
+            Color::Yellow => Color::Red
+        }
+    }
+
+    pub fn favour_mod(self) -> f64 {
+        match self {
+            Color::Red => 1.,
+            Color::Yellow => -1.
+        }
+    }
 }
--- minmax/src/main.rs
+++ minmax/src/main.rs
@@ -1,3 +1,143 @@
+use connect4::{Color, Position, Slot};
+
 fn main() {
-    println!("Hello, world!");
+    let board = Position::default()
+        .with_move(Color::Red, 3)
+        .with_move(Color::Yellow, 0)
+        .with_move(Color::Red, 2)
+        // .with_move(Color::Yellow, 0)
+        // .with_move(Color::Red, 4)
+        .with_move(Color::Yellow, 0);
+
+    println!("best move: {}", minmax(&board, Color::Red, 2).0);
+}
+
+fn minmax(board: &Position, turn: Color, depth: u32) -> (usize, f64) {
+    let mut best = (69, std::f64::NEG_INFINITY * turn.favour_mod());
+    for mve in board.allowed_moves() {
+        let new_board = board.with_move(turn, mve);
+        let favour = red_favour(&new_board);
+        let score = if depth > 0 {
+            if favour == std::f64::INFINITY * turn.favour_mod() {
+                println!("ah ok {:?} {}", turn, depth);
+                return (mve, favour)
+            }
+            minmax(&board.with_move(turn, mve), turn.opposite(), depth - 1).1
+        } else {
+            favour
+        };
+
+        if score > best.1 * turn.favour_mod() {
+            println!("{:?} is better than {:?}", (mve, score), best);
+            best = (mve, score);
+        }
+    }
+
+    best
+}
+
+fn red_favour(position: &Position) -> f64 {
+    let mut total_favour = 0.; // red is pos, yellow is neg
+
+    // sideways
+    for r in 0..6 {
+        let mut scan = ScanState::default();
+        for c in 0..7 {
+            if scan.eat(position.get(r, c)) {
+                break;
+            }
+        }
+        let score = scan.score();
+        match score {
+            Score::Winner(Color::Red) => {
+                return std::f64::INFINITY
+            },
+            Score::Winner(Color::Yellow) => {
+                return std::f64::NEG_INFINITY
+            }
+            Score::Favour(favour) => {
+                total_favour += favour;
+            },
+        }
+    }
+
+    // vertical
+    for c in 0..7 {
+        let mut scan = ScanState::default();
+        for r in 0..6 {
+            if scan.eat(position.get(r, c)) {
+                break;
+            }
+        }
+        let score = scan.score();
+        match score {
+            Score::Winner(Color::Red) => {
+                return std::f64::INFINITY
+            },
+            Score::Winner(Color::Yellow) => {
+                return std::f64::NEG_INFINITY
+            }
+            Score::Favour(favour) => {
+                total_favour += favour;
+            },
+        }
+    }
+
+    total_favour
+}
+
+#[derive(Default)]
+struct ScanState {
+    pub winner: Option<Color>,
+    pub favour: f64,
+    slot_window: [Slot; 4],
+    slot_fill_index: usize,
+}
+
+impl ScanState {
+    fn eat(&mut self, slot: Slot) -> bool {
+        if self.slot_fill_index < 4 {
+            self.slot_window[self.slot_fill_index] = slot;
+            self.slot_fill_index += 1;
+            if self.slot_fill_index < 4 {
+                return false;
+            }
+        } else {
+            self.slot_window[0] = self.slot_window[1];
+            self.slot_window[1] = self.slot_window[2];
+            self.slot_window[2] = self.slot_window[3];
+            self.slot_window[3] = slot;
+        }
+
+        let red_cnt = self.slot_window.iter().fold(0, |acc, x| acc + (x == &Slot::Color(Color::Red)) as usize);
+        let ylw_cnt = self.slot_window.iter().fold(0, |acc, x| acc + (x == &Slot::Color(Color::Yellow)) as usize);
+
+        if red_cnt == 4 {
+            self.winner = Some(Color::Red);
+            return true;
+        }
+        if ylw_cnt == 4 {
+            self.winner = Some(Color::Yellow);
+            return true;
+        }
+        if red_cnt > 0 && ylw_cnt == 0 {
+            self.favour += (red_cnt.pow(3) as f64) / 27.
+        } else if ylw_cnt > 0 && red_cnt == 0 {
+            self.favour -= (ylw_cnt.pow(3) as f64) / 27.
+        }
+        false
+    }
+
+    fn score(&self) -> Score {
+        if let Some(winner) = self.winner {
+            Score::Winner(winner)
+        } else {
+            Score::Favour(self.favour)
+        }
+    }
+}
+
+enum Score {
+    Winner(Color),
+    Favour(f64)
 }
